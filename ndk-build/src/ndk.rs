rust-mobile/cargo-apk

commit 15d7c2359642092af0a63f8c84aaf5e11dd686a4

ndk-build: Point CXX_<triple> to <triple>-clang++ from the ndk (#18)
Cross-compiling with build.rs using cc::Build and .use_cpp(true) (for
example) invokes <triple>-clang++ which is usually not on PATH or known
to point to the correct NDK. Set the CXX env var for the requested
triple to ensure the right C++ compiler is available and used.

ndk-build/src/ndk.rs
@@ -170,22 +170,25 @@ impl Ndk {
         Ok(toolchain_dir)
     }
 
-    pub fn clang(&self, target: Target, platform: u32) -> Result<PathBuf, NdkError> {
+    pub fn clang(&self, target: Target, platform: u32) -> Result<(PathBuf, PathBuf), NdkError> {
         #[cfg(target_os = "windows")]
         let ext = ".cmd";
         #[cfg(not(target_os = "windows"))]
         let ext = "";
 
-        let clang = self.toolchain_dir()?.join("bin").join(format!(
-            "{}{}-clang{}",
-            target.ndk_llvm_triple(),
-            platform,
-            ext
-        ));
+        let bin_name = format!("{}{}-clang{}", target.ndk_llvm_triple(), platform, ext);
+
+        let clang = self.toolchain_dir()?.join("bin").join(&bin_name);
         if !clang.exists() {
             return Err(NdkError::PathNotFound(clang));
         }
-        Ok(clang)
+
+        let clang_pp = clang.with_file_name(format!("{}++", &bin_name));
+        if !clang_pp.exists() {
+            return Err(NdkError::PathNotFound(clang_pp));
+        }
+
+        Ok((clang, clang_pp))
     }
 
     pub fn toolchain_bin(&self, bin: &str, target: Target) -> Result<PathBuf, NdkError> {

ndk-build/src/cargo.rs
@@ -7,8 +7,9 @@ pub fn cargo_apk(ndk: &Ndk, target: Target, sdk_version: u32) -> Result<Command,
     let triple = target.rust_triple();
     let mut cargo = Command::new("cargo");
 
-    let clang = ndk.clang(target, sdk_version)?;
+    let (clang, clang_pp) = ndk.clang(target, sdk_version)?;
     cargo.env(format!("CC_{}", triple), &clang);
+    cargo.env(format!("CXX_{}", triple), &clang_pp);
     cargo.env(cargo_env_target_cfg("LINKER", triple), &clang);
 
     let ar = ndk.toolchain_bin("ar", target)?;

commit 95dd5f40f185954ff25c2cf141ddf39350c594dd

[ndk-build] Fix paths to toolchain and clang on Windows (#30)
* ndk-build: Windows toolchain binaries are .exe, not wrapper scripts

All toolchain binaries are executable files with a .exe extension. Only
the clang compiler is wrapped in bash scripts (no extension) or batch
scripts (.cmd extension).
Update the extension to match this.

Note that this was tested on NDK r21c, version 21.2.6472646.

* ndk-build: Append extension after ++ in clang++

The ++ was mistakenly appeneded to the full filename of the binary (by
me). On Windows this binary resolves to clang.cmd and ++ is appened to
that for the C++ variant, resulting in clang.cmd++. This binary doesn't
exist, it should be clang++.cmd.

Fixes: 15d7c2359642092af0a63f8c84aaf5e11dd686a4

ndk-build/src/ndk.rs
@@ -176,14 +176,15 @@ impl Ndk {
         #[cfg(not(target_os = "windows"))]
         let ext = "";
 
-        let bin_name = format!("{}{}-clang{}", target.ndk_llvm_triple(), platform, ext);
+        let bin_name = format!("{}{}-clang", target.ndk_llvm_triple(), platform);
+        let bin_path = self.toolchain_dir()?.join("bin");
 
-        let clang = self.toolchain_dir()?.join("bin").join(&bin_name);
+        let clang = bin_path.join(format!("{}{}", &bin_name, ext));
         if !clang.exists() {
             return Err(NdkError::PathNotFound(clang));
         }
 
-        let clang_pp = clang.with_file_name(format!("{}++", &bin_name));
+        let clang_pp = bin_path.join(format!("{}++{}", &bin_name, ext));
         if !clang_pp.exists() {
             return Err(NdkError::PathNotFound(clang_pp));
         }
@@ -193,7 +194,7 @@ impl Ndk {
 
     pub fn toolchain_bin(&self, bin: &str, target: Target) -> Result<PathBuf, NdkError> {
         #[cfg(target_os = "windows")]
-        let ext = ".cmd";
+        let ext = ".exe";
         #[cfg(not(target_os = "windows"))]
         let ext = "";
 