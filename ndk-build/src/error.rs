1948911b34cfe7a7af8e56b477e59ce6c0aeb99e
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-02-05 22:05:06 +0000

    Report error better (when encountered weird ndk installation)

--- ndk-build/src/error.rs
+++ ndk-build/src/error.rs
@@ -8,13 +8,13 @@ use thiserror::Error;
 pub enum NdkError {
     #[error(
         "Android SDK is not found. \
-    Please set the path to the Android SDK with the $ANDROID_SDK_ROOT \
+    Please set the path to the Android SDK with the $ANDROID_HOME \
     environment variable."
     )]
     SdkNotFound,
     #[error(
         "Android NDK is not found. \
-        Please set the path to the Android NDK with $ANDROID_NDK_ROOT \
+        Please set the path to the Android NDK with $NDK_HOME \
         environment variable."
     )]
     NdkNotFound,
